6c0cf3b1fac44f2522e418d883de4a16390599cd
Lucas Messenger <lucas.c.messenger@gmail.com>
2020-12-10 21:07:14 -0500

    Day 8 part 2 complete

--- day8/run.py
+++ day8/run.py
@@ -1,34 +1,53 @@
 #!/usr/bin/env python3
 
-
 def accumulator(_input):
     """
     do action
     """
     accum = 0
     counter = set()
+    status = True
     i = 0
     while i < len(_input):
         if (i, _input[i]) in counter:
-            print('infinite loop')
+            status = False
+            #print('infinite loop')
             break
-        act, num = _input[i]
+
+        act, num = _input[i].split()
+        counter.add((i, _input[i]))
         if act == 'acc':
             accum += int(num)
+            i += 1
         elif act == 'jmp':
             i += int(num)
+        else:
+            i += 1
+
+    return accum, status
+
+
+def non_infinite(_input):
+    """
+    find non infinite loops
+    """
+    _input.copy()
+    for idx, line in enumerate(_input):
+        new_data = _input.copy()
+
+        action, _ = line.split()
+        if action == 'jmp':
+            new_data[idx] = line.replace('jmp', 'nop')
+        elif action == 'nop':
+            new_data[idx] = line.replace('nop', 'jmp')
+        else:
             continue
-        counter.add((i, _input[i]))
-        i += 1
 
-    return accum
+        # walrus operator, anyone?
+        if (result := accumulator(new_data))[1]:
+            return result[1]
 
-
-def get_actions(data):
-    """
-    get actions from file
-    """
-    return [tuple(line.split(' ', 1)) for line in data]
+    return 0
 
 
 def main():
@@ -36,9 +55,10 @@ def main():
     main
     """
     with open('input.txt') as fp:
-        data = get_actions(fp.read().splitlines())
+        data = fp.read().splitlines()
 
-    print('accumulator', accumulator(data))
+    print('part 1 accumulator:', accumulator(data)[0])
+    print('part 2 boot fix:', non_infinite(data)[0])
 
 
 if __name__ == '__main__':
